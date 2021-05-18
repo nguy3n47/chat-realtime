0917414fee4e473dd003329fa08e5c4bfe8146c1
nguy3n47 <nguyen.fit.hcmus@outlook.com>
2021-05-18 14:14:22 +0700

    update

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // Setup basic express server
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
 // Routing
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Chat Room
 
 let numUsers = 0;
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
   let addedUser = false;
   // when the client emits 'add user', this listens and executes
-  socket.on("add user", (data) => {
+  socket.on('add user', (data) => {
     if (addedUser) return;
     socket.username = data.username;
     socket.avatar = data.avatar;
     ++numUsers;
     addedUser = true;
-    io.emit("login", {
+    io.emit('login', {
       numUsers: numUsers,
     });
 
-    socket.broadcast.emit("user joined", {
+    socket.broadcast.emit('user joined', {
       username: socket.username,
       numUsers: numUsers,
     });
   });
 
   // when the client emits 'new message', this listens and executes
-  socket.on("new message", (data) => {
+  socket.on('new message', (data) => {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit("new message", {
+    socket.broadcast.emit('new message', {
       username: socket.username,
       avatar: socket.avatar,
       message: data,
@@ -44,13 +44,13 @@ io.on("connection", (socket) => {
   });
 
   // when the user disconnects.. perform this
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
     if (addedUser) {
       --numUsers;
 
       // echo globally that this client has left
-      socket.broadcast.emit("user left", {
+      socket.broadcast.emit('user left', {
         username: socket.username,
         numUsers: numUsers,
       });
@@ -59,5 +59,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log("Server listening at port %d", port);
+  console.log(`🚀 Server listening at ${port}`);
 });
--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
 $(() => {
   // Initialize variables
   const $window = $(window);
-  const $usernameInput = $(".usernameInput"); // Input for username
-  const $messages = $(".messages"); // Messages area
-  const $inputMessage = $(".inputMessage"); // Input message input box
+  const $usernameInput = $('.usernameInput'); // Input for username
+  const $messages = $('.messages'); // Messages area
+  const $inputMessage = $('.inputMessage'); // Input message input box
 
-  const $loginPage = $(".login.page"); // The login page
-  const $chatPage = $(".chat.page"); // The chatroom page
+  const $loginPage = $('.login.page'); // The login page
+  const $chatPage = $('.chat.page'); // The chatroom page
 
   const socket = io();
 
@@ -19,50 +19,50 @@ $(() => {
   const setUser = () => {
     username = $usernameInput.val().trim();
     avatar = `https://ui-avatars.com/api/?background=random&name=${username.charAt(
-      0,
+      0
     )}`;
 
     // If the username is valid
     if (username) {
       $loginPage.fadeOut();
       $chatPage.show();
-      $loginPage.off("click");
+      $loginPage.off('click');
       $currentInput = $inputMessage.focus();
 
       // Tell the server your username
-      socket.emit("add user", { username, avatar });
+      socket.emit('add user', { username, avatar });
     }
   };
 
   const addParticipantsMessage = (data) => {
-    let message = "Welcome to Socket.IO Chat – ";
+    let message = 'Welcome to Socket.IO Chat – ';
     if (data.numUsers === 1) {
       message += `there's 1 participant`;
     } else {
       message += `there are ${data.numUsers} participants`;
     }
-    const $el = $("<li>").addClass("log").text(message);
-    if ($(".log")[0]) {
-      $(".log").eq(0).text(message);
+    const $el = $('<li>').addClass('log').text(message);
+    if ($('.log')[0]) {
+      $('.log').eq(0).text(message);
     } else {
       $messages.append($el);
     }
   };
 
-  socket.on("login", (data) => {
+  socket.on('login', (data) => {
     addParticipantsMessage(data);
   });
 
-  socket.on("user joined", (data) => {
+  socket.on('user joined', (data) => {
     let message = `${data.username} joined`;
-    const $el = $("<li>").addClass("log").text(message);
+    const $el = $('<li>').addClass('log').text(message);
     $messages.append($el);
   });
 
-  socket.on("user left", (data) => {
+  socket.on('user left', (data) => {
     addParticipantsMessage(data);
     let message = `${data.username} left`;
-    const $el = $("<li>").addClass("log").text(message);
+    const $el = $('<li>').addClass('log').text(message);
     $messages.append($el);
   });
 
@@ -71,37 +71,37 @@ $(() => {
     let message = $inputMessage.val();
     // if there is a non-empty message and a socket connection
     if (message) {
-      $inputMessage.val("");
+      $inputMessage.val('');
       addChatMessage({ username, avatar, message });
       // tell server to execute 'new message' and send along one parameter
-      socket.emit("new message", message);
+      socket.emit('new message', message);
     }
   };
 
   const addChatMessage = (data) => {
     const $avatarDiv = $(
-      `<img class="avatar" src=${data.avatar} width="64" height="64" />`,
+      `<img class="avatar" src=${data.avatar} width="64" height="64" />`
     );
     const $usernameDiv = $('<span class="username"/>').text(data.username);
     const $messageBodyDiv = $('<span class="messageBody">').text(data.message);
 
     const $messageDiv = $('<li class="message"/>')
-      .data("username", data.username)
+      .data('username', data.username)
       .append(
         $avatarDiv,
-        $('<div class="content"/>').append($usernameDiv, $messageBodyDiv),
+        $('<div class="content"/>').append($usernameDiv, $messageBodyDiv)
       );
 
     $messages.append($messageDiv);
     $messages[0].scrollTop = $messages[0].scrollHeight;
   };
 
-  socket.on("new message", (data) => {
+  socket.on('new message', (data) => {
     addChatMessage(data);
   });
 
-  socket.on("disconnect", () => {
-    log("you have been disconnected");
+  socket.on('disconnect', () => {
+    log('you have been disconnected');
   });
 
   // Click events
